9676de9f3d1841c806cd0da55ba569c1e109bb67
MalavikaAjit <malavikaajit9@gmail.com>
2021-09-25 02:15:40 +0530

    [Malavika]:Refactored code for responsiveness

--- project/javascript/script.js
+++ project/javascript/script.js
@@ -46,43 +46,43 @@ function closeNotes() {
   document.getElementById("display-icon-main").innerHTML = "";
   document.getElementById("searchValue").value = "";
   document.getElementById("display-avatar").style.display = "none";
-  
-  
 }
 
 const placeholder = document.querySelector("#placeholder");
 const notesField = document.querySelector("#notes");
 
 function displayNotes(newnote) {
-
-  var nHTML = '';
+  var nHTML = "";
   notes = newnote;
-  console.log("notesssss",notes);
+  console.log("notesssss", notes);
   for (let i = 0; i < notes.length; i++) {
     if (notes[i].isDeleted == false && notes[i].isArchived == false) {
-
       let displayEmail = [];
       let resCollaberators = [];
 
       resCollaberators = notes[i].collaborators;
       if (resCollaberators !== undefined && resCollaberators.length > 0) {
         for (let j = 0; j < resCollaberators.length; j++) {
-          displayEmail.push(resCollaberators[j].email)
+          displayEmail.push(resCollaberators[j].email);
         }
       }
       let colHTML = ``;
       for (let j = 0; j < displayEmail.length; j++) {
-        colHTML += `<div style="list-style-type:none" class="display-collab-letter mr-2">` + displayEmail[j].charAt(0).toUpperCase() + `</div>`
+        colHTML +=
+          `<div style="list-style-type:none" class="display-collab-letter mr-2">` +
+          displayEmail[j].charAt(0).toUpperCase() +
+          `</div>`;
       }
-      nHTML += `<div class="note" style="background: ${notes[i].color};">
+      nHTML +=
+        `<div class="note" style="background: ${notes[i].color};">
       
         <div class="note-title"  class="btn btn-primary" data-toggle="modal" data-target="#exampleModal2" onclick=" openPopUp('${i}')">${notes[i].title}</div>
         <div class="note-text">${notes[i].description}</div>` +
         `<div class="display-collab-icon">
-                      <div class="display-icon" >` + colHTML +
+                      <div class="display-icon" >` +
+        colHTML +
         `</div>
                     </div>` +
-
         ` <div class="toolbar-container">
                  <div class="toolbar">         
                  <i class="fas fa-trash" id="${notes[i].id}" onclick="deleteNote(id)"></i>  
@@ -129,12 +129,10 @@ function displayNotes(newnote) {
 }
 displayNotes(newnote);
 
-
-function collabPopup(i){
-  
-  console.log("hellooo",i);
-document.getElementById("collabDisplayNotes").innerHTML ="";
- collabModalForDisplayNotesHTML =`
+function collabPopup(i) {
+  console.log("hellooo", i);
+  document.getElementById("collabDisplayNotes").innerHTML = "";
+  collabModalForDisplayNotesHTML = `
 <div class="modal fade" id="exampleModal3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
       aria-hidden="true">
       <div class="modal-dialog collab_box" role="document">
@@ -179,28 +177,56 @@ document.getElementById("collabDisplayNotes").innerHTML ="";
         </div>
       </div>
     </div>
-`
-document.getElementById("collabDisplayNotes").innerHTML = collabModalForDisplayNotesHTML;
+`;
+  document.getElementById("collabDisplayNotes").innerHTML =
+    collabModalForDisplayNotesHTML;
 }
 
-
-function displayArchiveNotes(displaynote) { 
+function displayArchiveNotes(displaynote) {
+  // const notes = displaynote;
+  // const hasNotes = notes.length > 0;
+  // placeholder.style.display = hasNotes ? "none" : "flex";
+  // notesField.innerHTML = notes
+  //   .map(
+  var nHTML = "";
   const notes = displaynote;
-  const hasNotes = notes.length > 0;
-  placeholder.style.display = hasNotes ? "none" : "flex";
-  notesField.innerHTML = notes
-    .map(
-      (note) => `
+  console.log("notesssssarch", notes);
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].isArchived == true) {
+      let displayEmail = [];
+      let resCollaberators = [];
+
+      resCollaberators = notes[i].collaborators;
+      if (resCollaberators !== undefined && resCollaberators.length > 0) {
+        for (let j = 0; j < resCollaberators.length; j++) {
+          displayEmail.push(resCollaberators[j].email);
+        }
+      }
+      let colHTML = ``;
+      for (let j = 0; j < displayEmail.length; j++) {
+        colHTML +=
+          `<div style="list-style-type:none" class="display-collab-letter mr-2">` +
+          displayEmail[j].charAt(0).toUpperCase() +
+          `</div>`;
+      }
+      nHTML +=
+        `
      
-     <div class="note" style="background: ${note.color}; ">   
-     <div class="note-title">${note.title}</div>
-     <div class="note-text">${note.description}</div>          
+     <div class="note" style="background: ${notes[i].color}; ">   
+     <div class="note-title">${notes[i].title}</div>
+     <div class="note-text">${notes[i].description}</div>  ` +
+        `<div class="display-collab-icon">
+                   <div class="display-icon" >` +
+        colHTML +
+        `</div>
+                 </div>` +
+        `        
      <div class="toolbar-container">
        <div class="toolbar">         
-       <i class="fas fa-trash" id="${note.id}" onclick="deleteNote(id)"></i>  
+       <i class="fas fa-trash" id="${notes[i].id}" onclick="deleteNote(id)"></i>  
       
        <i class="material-icons">more_vert</i> 
-       <i class="material-icons"  id="${note.id}" onclick="unArchiveNote(id)">archive</i>
+       <i class="material-icons"  id="${notes[i].id}" onclick="unArchiveNote(id)">archive</i>
        <i class="material-icons">palette</i> 
        <i class="material-icons">person_add_alt</i> 
        <i class="material-icons">add_alert</i>       
@@ -208,36 +234,64 @@ function displayArchiveNotes(displaynote) {
      </div>
    </div>
      
-   `
-    )
-    .join("");
+   `;
+    }
+  }
+  notesField.innerHTML = nHTML;
 }
 
 function displayTrashNotes(displaynote) {
   // const notes = JSON.parse(localStorage.getItem('archiveNotes')) || [];
+  // const notes = displaynote;
+  // const hasNotes = notes.length > 0;
+  // placeholder.style.display = hasNotes ? "none" : "flex";
+  // notesField.innerHTML = notes
+  //   .map(
+  var nHTML = "";
   const notes = displaynote;
-  const hasNotes = notes.length > 0;
-  placeholder.style.display = hasNotes ? "none" : "flex";
-  notesField.innerHTML = notes
-    .map(
-      (note) => `
+  console.log("notesssssarch", notes);
+  for (let i = 0; i < notes.length; i++) {
+    if (notes[i].isDeleted == true) {
+      let displayEmail = [];
+      let resCollaberators = [];
+
+      resCollaberators = notes[i].collaborators;
+      if (resCollaberators !== undefined && resCollaberators.length > 0) {
+        for (let j = 0; j < resCollaberators.length; j++) {
+          displayEmail.push(resCollaberators[j].email);
+        }
+      }
+      let colHTML = ``;
+      for (let j = 0; j < displayEmail.length; j++) {
+        colHTML +=
+          `<div style="list-style-type:none" class="display-collab-letter mr-2">` +
+          displayEmail[j].charAt(0).toUpperCase() +
+          `</div>`;
+      }
+      nHTML +=
+        `
      
-    <div class="note" style="background: ${note.color}; ">
-     <div class="note-title">${note.title}</div>
-     <div class="note-text">${note.description}</div>          
+    <div class="note" style="background:${notes[i].color}; ">
+     <div class="note-title">${notes[i].title}</div>
+     <div class="note-text">${notes[i].description}</div>  ` +
+        `<div class="display-collab-icon">
+                   <div class="display-icon" >` +
+        colHTML +
+        `</div>
+                 </div>` +
+        `           
      <div class="toolbar-container">
        <div class="toolbar"  style="float:right;">         
-       <i class="fas fa-trash-restore" id="${note.id}" onclick="restoreTrashNote(id)"></i>     
+       <i class="fas fa-trash-restore" id="${notes[i].id}" onclick="restoreTrashNote(id)"></i>     
        </div>
      </div>
    </div>
      
-   `
-    )
-    .join("");
+   `;
+    }
+  }
+  notesField.innerHTML = nHTML;
 }
-
-
 
 function colourChange(colorCode) {
   document.getElementById("form-container").style.backgroundColor = colorCode;
@@ -253,7 +307,7 @@ function removeTextBox() {
 $(".display-avatar").hide();
 function removeAvatar() {
   var searchField = document.getElementById("searchValue").value;
-  if (searchField !== '') {
+  if (searchField !== "") {
     $(".display-avatar").show();
   }
 }
@@ -279,18 +333,22 @@ function openPopUp(i) {
   resCollaberators = notes[i].collaborators;
   if (resCollaberators !== undefined && resCollaberators.length > 0) {
     for (let j = 0; j < resCollaberators.length; j++) {
-      displayEmail.push(resCollaberators[j].email)
+      displayEmail.push(resCollaberators[j].email);
     }
   }
   console.log("hey hey", displayEmail);
   let colHTML = ``;
   for (let j = 0; j < displayEmail.length; j++) {
-    colHTML += `<div style="list-style-type:none" class="display-collab-letter mr-2">` + displayEmail[j].charAt(0).toUpperCase() + `</div>`
+    colHTML +=
+      `<div style="list-style-type:none" class="display-collab-letter mr-2">` +
+      displayEmail[j].charAt(0).toUpperCase() +
+      `</div>`;
   }
   // console.log(color);
   popUp.innerHTML = "";
   if (notes[i].title != null) {
-    popUp.innerHTML = `
+    popUp.innerHTML =
+      `
 <div class="modal fade " id="exampleModal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
       aria-hidden="true" >
       <div class="modal-dialog collab_box" role="document">
@@ -315,16 +373,19 @@ function openPopUp(i) {
             </div>
           </div>
           <div class="display-collab-icon">
-                      <div class="display-icon-popup" >` + colHTML +
+                      <div class="display-icon-popup mb-2"  >` +
+      colHTML +
       `</div>
             </div>
           <div class="modal-footer collab_footer_popup">
+          <div class="row" style="width:100%">
+            <div class="col-6">
             <div class="toolbar_popup">
               <i class="material-icons">more_vert</i>
               <i class="material-icons" id="${notes[i].id}" onclick="archiveNote(id)" data-dismiss="modal" >archive</i>
               <i class="material-icons  dropdown "  id="dropdownMenuButton" data-toggle="dropdown"  aria-haspopup="true" aria-expanded="false">
               palette
-
+         
               <div class="dropdown-menu color-tooltip" aria-labelledby="dropdownMenuButton">
           <span class="color_drop">
                       <a class="dropdown-item color-option" href="#" onclick="colourChangeNote(this.id,'#ffffff')"id="${notes[i].id}" style="background-color:#ffffff;"></a>
@@ -356,9 +417,14 @@ function openPopUp(i) {
               <i class="material-icons" type="button" class="btn btn-primary" data-toggle="modal"  data-dismiss="modal"  data-target="#exampleModal3"  onclick="collabPopup('${notes[i].id}')">person_add_alt</i>
               <i class="material-icons">add_alert</i>
             </div>
+            </div>
+            <div class="col-6">
             <div class="savebtn_popup">
               <button type="button" class="btn btn-secondary btn_cancel" data-dismiss="modal" onclick="updateNotes('${notes[i].id}')">Save</button>
             </div>
+            </div>
+            </div>
+            
 
           </div>
         </div>
@@ -373,5 +439,3 @@ function openPopUp(i) {
 $("#myModal").on("shown.bs.modal", function () {
   $("#myInput").trigger("focus");
 });
-
-
